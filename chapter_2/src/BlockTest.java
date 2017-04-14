87c5801bf20876f0ba82407b5e5a33148105fcb1
Paul Valle <paul.valle@datadisk.co.uk>
2017-04-14 18:11:08 +0100

    finished chapter 2 - pfv

--- chapter_2/src/BlockTest.java
+++ chapter_2/src/BlockTest.java
@@ -10,6 +10,7 @@ public class BlockTest {
         for (x = 0; x < 10; x++) {
             System.out.println("This is x: " + x);
             System.out.println("This is y: " + y);
-            y = y - 2;        }
+            y = y - 2;
+        }
     }
 }
