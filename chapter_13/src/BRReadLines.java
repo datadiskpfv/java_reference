09b258043274c9dcd702c0634adb69b44048177d
Paul Valle <paul.valle@datadisk.co.uk>
2017-05-07 20:23:20 +0100

    started chapter 13, file i/o and applets - pfv

---
+++ chapter_13/src/BRReadLines.java
@@ -0,0 +1,22 @@
+/**
+ * Created by vallep on 5/7/2017.
+ *
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BRReadLines {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+        System.out.println("Enter lines of text.");
+        System.out.println("Enter 'stop' to quit: ");
+        do {
+            str = br.readLine();
+            System.out.println(str);
+    } while (!str.equals("stop"));
+    }
+}
